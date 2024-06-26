b7b09f48cadc523e353a4d19a458f50082633f81
oykhodjayev1 <abdulazizoyxojayev453@gmail.com>
2024-06-26 21:28:47 +0500

    pages create

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Typography, Card } from "@material-tailwind/react";
 import { Route, Routes } from "react-router-dom";
+import MetroPage from "./pages/metro";
 
 export default function App() {
   return (
+    <>
+    <MetroPage/>
     <Routes>
       <Route path="/" />
     </Routes>
+    </>
+    
   );
 }
---
+++ src/pages/metro.jsx
@@ -0,0 +1,11 @@
+import "./metro.css";
+
+export default function MetroPage(){
+    return(
+        <>
+        <div className="navbar">
+            
+        </div>
+        </>
+    )
+}
